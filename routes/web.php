8097dbca39a347b7fab2444296156cac3a801138
Devadatta Sahoo <webmaster@nettantra.com>
2022-10-27 11:30:19 +0530

    Updates

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Filament\Resources\AppraisalResponseResource\Pages\CreateAppraisalResponse;
+use App\Http\Controllers\AppraisalResponseRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('filament.auth.login'));
 });
