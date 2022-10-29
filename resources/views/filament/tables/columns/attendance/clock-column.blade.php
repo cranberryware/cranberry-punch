1af4c64e8baa6f344da564c50e614272d9dc23cf
Devadatta Sahoo <webmaster@nettantra.com>
2022-10-29 06:46:23 +0530

    Updates

--- resources/views/filament/tables/columns/attendance/clock-column.blade.php
+++ resources/views/filament/tables/columns/attendance/clock-column.blade.php
@@ -1,9 +1,9 @@
-<div class="px-4 py-3">
-    @if (!empty($getRecord()->{$getName()}))
+@if ($getRecord()->{$getName()})
+    <div class="px-4 py-3">
         <div class="text">
             {{ \Carbon\Carbon::parse($getRecord()->{$getName()})->tz(config('app.user_timezone'))->format(config('app.user_datetime_format')) }}
         </div>
         <div class="text-sm text-gray-400"><strong>From:</strong> {{ $getRecord()->{$getName() . '_location'} }}</div>
         <div class="text-sm text-gray-400"><strong>IP Address:</strong> {{ $getRecord()->{$getName() . '_ip'} }}</div>
-    @endif
-</div>
+    </div>
+@endif
