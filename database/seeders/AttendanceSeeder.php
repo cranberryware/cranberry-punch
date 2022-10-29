2d77a89c9dba7f6f2d8ac21962dbaf8ea9fdce9a
Devadatta Sahoo <webmaster@nettantra.com>
2022-10-29 10:26:22 +0530

    Updates

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -24,11 +24,13 @@ class AttendanceSeeder extends Seeder
                 $start_check_out = \Carbon\Carbon::parse(now()->subDays($i)->toDateString() . " 17:00:00");
                 $end_check_out = \Carbon\Carbon::parse(now()->subDays($i)->toDateString() . " 20:00:00");
                 $employee = Employee::inRandomOrder()->first();
+                $end_check_out_randomizer = [null, $end_check_out, $end_check_out, $end_check_out, $end_check_out];
+                shuffle($end_check_out_randomizer);
                 $attendance = Attendance::create([
                     'user_id' => $employee->user_id,
                     'employee_id' => $employee->id,
                     'check_in' => $faker->dateTimeBetween($start_check_in, $end_check_in),
-                    'check_out' => $faker->dateTimeBetween($start_check_out, $end_check_out),
+                    'check_out' => $end_check_out_randomizer,
                 ]);
             }
         }
