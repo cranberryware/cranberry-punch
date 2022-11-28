3f359e9aa1ffe7b184069ada3d7e179e54de1449
Manish Das <manish.das@nettantra.net>
2022-11-28 15:33:26 +0530

    removed the attribute inheritance inside the blade file attributes

--- resources/views/filament/widgets/attendance-calendar.blade.php
+++ resources/views/filament/widgets/attendance-calendar.blade.php
@@ -1,6 +1,5 @@
 <x-filament::widget 
-:attributes="\Filament\Support\prepare_inherited_attributes($xattributes)->merge(['class'=>'filament-widgets-table-widget oa-attendance-calendar-widget'])"
-
+class="filament-widgets-table-widget oa-attendance-calendar-widget"
 >
     {{ $this->table }}
 </x-filament::widget>
