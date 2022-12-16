aecbe03943dbddcd221d1331af94cba3baf0f588
Mohommed Motahir Alam <mohommedmotahir.alam@nettantra.net>
2022-12-16 11:21:34 +0530

    extend login

--- app/Filament/Auth/Login.php
+++ app/Filament/Auth/Login.php
@@ -3,71 +3,12 @@
 namespace App\Filament\Auth;
 
 
-use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
-use DanHarrin\LivewireRateLimiting\WithRateLimiting;
-use Filament\Facades\Filament;
-use Filament\Forms\ComponentContainer;
-use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
-use Filament\Http\Responses\Auth\Contracts\LoginResponse;
-use Illuminate\Contracts\View\View;
-use Illuminate\Validation\ValidationException;
-use Livewire\Component;
+use Filament\Forms\Components\Checkbox;
+use Filament\Http\Livewire\Auth\Login as BasePage;
 
-/**
- * @property ComponentContainer $form
- */
-class Login extends Component implements HasForms
+class Login extends BasePage
 {
-    use InteractsWithForms;
-    use WithRateLimiting;
-
-    public $email = '';
-
-    public $password = '';
-
-    public $remember = false;
-
-    public function mount(): void
-    {
-        if (Filament::auth()->check()) {
-            redirect()->intended(Filament::getUrl());
-        }
-
-        $this->form->fill();
-    }
-
-    public function authenticate(): ?LoginResponse
-    {
-        try {
-            $this->rateLimit(5);
-        } catch (TooManyRequestsException $exception) {
-            throw ValidationException::withMessages([
-                'email' => __('filament::login.messages.throttled', [
-                    'seconds' => $exception->secondsUntilAvailable,
-                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
-                ]),
-            ]);
-        }
-
-        $data = $this->form->getState();
-
-        if (!Filament::auth()->attempt([
-            'email' => $data['email'],
-            'password' => $data['password'],
-        ], $data['remember'])) {
-            throw ValidationException::withMessages([
-                'email' => __('filament::login.messages.failed'),
-            ]);
-        }
-
-        session()->regenerate();
-
-        return app(LoginResponse::class);
-    }
-
     protected function getFormSchema(): array
     {
         return [
@@ -85,14 +26,5 @@ class Login extends Component implements HasForms
             Checkbox::make('remember')
                 ->label(__('filament::login.fields.remember.label')),
         ];
-    }
-
-    public function render(): View
-    {
-        return view('filament::login')
-            ->layout('filament::components.layouts.card', [
-                'title' => __('filament::login.title'),
-                'class' => 'abc',
-            ]);
     }
 }
