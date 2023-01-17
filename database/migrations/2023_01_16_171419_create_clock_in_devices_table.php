6940adfc484536125138bcd3d96013db4169dbb4
Devadatta Sahoo <webmaster@nettantra.com>
2023-01-17 13:57:24 +0530

    Adding ClockInDevice Model

---
+++ database/migrations/2023_01_16_171419_create_clock_in_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clock_in_devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('device_name');
+            $table->string('device_location');
+            $table->string('device_serial')->unique();
+            $table->string('device_identifier')->unique();
+            $table->string('device_status');
+            $table->string('device_mode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clock_in_devices');
+    }
+};
