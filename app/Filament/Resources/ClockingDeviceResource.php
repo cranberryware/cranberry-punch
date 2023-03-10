67b8f863ccdf718abe37326ec232f4022893b0e1
Devadatta Sahoo <webmaster@nettantra.com>
2023-03-10 22:50:31 +0530

    Changing ClockIn to Clocking

--- app/Filament/Resources/ClockingDeviceResource.php
+++ app/Filament/Resources/ClockingDeviceResource.php
@@ -23,7 +23,7 @@ class ClockInDeviceResource extends Resource
 {
     protected static ?string $model = ClockInDevice::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-finger-print';
 
     protected static function getNavigationGroup(): ?string
     {
