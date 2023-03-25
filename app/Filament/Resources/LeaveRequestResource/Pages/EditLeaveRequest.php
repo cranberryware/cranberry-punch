9d997f00840244d9fa2419c41fce6bcdcf36b9fe
@santoshk.jena <santoshk.jena@nettantra.net>
2023-03-25 11:47:36 +0530

    Added action button in the leave request edit page

--- app/Filament/Resources/LeaveRequestResource/Pages/EditLeaveRequest.php
+++ app/Filament/Resources/LeaveRequestResource/Pages/EditLeaveRequest.php
@@ -2,13 +2,16 @@
 
 namespace App\Filament\Resources\LeaveRequestResource\Pages;
 
+use App\Enums\LeaveRequestStatus;
 use App\Filament\Resources\LeaveRequestResource;
 use App\Models\Employee;
 use App\Models\LeaveBalance;
+use App\Models\LeaveRequest;
 use App\Models\LeaveType;
 use Carbon\Carbon;
 use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
+use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
@@ -39,6 +42,39 @@ class EditLeaveRequest extends EditRecord
         ];
     }
 
+    protected function getFormActions(): array
+    {
+
+        return [
+            Action::make('save')
+                ->submit('save'),
+            Action::make('cancel')
+                ->url($this->previousUrl ?? static::getResource()::getUrl()),
+            Action::make('accept')
+                ->action(function () {
+                    $newRecord = $this->record;
+                    $newRecord->notes = $this->data['notes'];
+                    $newRecord->status = LeaveRequestStatus::APPROVED()->value;
+                    $newRecord->save();
+                }),
+            Action::make('reject')
+                ->action(function () {
+                    $newRecord = $this->record;
+                    $newRecord->notes = $this->data['notes'];
+                    $newRecord->status = LeaveRequestStatus::REJECTED()->value;
+                    $newRecord->save();
+                }),
+            Action::make('canceled')
+                ->action(function () {
+                    $newRecord = $this->record;
+                    $newRecord->notes = $this->data['notes'];
+                    $newRecord->status = LeaveRequestStatus::CANCELLED()->value;
+                    $newRecord->save();
+                })
+
+        ];
+    }
+
     protected function afterSave(): void
     {
         if ($this->record->id && $this->record->status == 'approved') {
@@ -55,21 +91,21 @@ class EditLeaveRequest extends EditRecord
             $number_of_allowance = $filtered->pluck('number_of_allowance')->first();
 
             //check the employee_id, leave_type_id and leave_session_id in LeaveBalance model.
-            $leaveBalance =  LeaveBalance::where(['employee_id'=> $this->record->employee_id, 'leave_type_id' => $this->record->leave_type_id, 'leave_session_id' => $this->record->leave_session_id])->first();
+            $leaveBalance =  LeaveBalance::where(['employee_id' => $this->record->employee_id, 'leave_type_id' => $this->record->leave_type_id, 'leave_session_id' => $this->record->leave_session_id])->first();
             if ($leaveBalance) {
                 //update
                 $leaveBalance->used = $leaveBalance->used + $this->record->duration;
                 $leaveBalance->available = $leaveBalance->available - $this->record->duration;
                 $leaveBalance->save();
             } else {
-               //create
-               $leaveBalance = new LeaveBalance();
-               $leaveBalance->employee_id = $this->record->employee_id;
-               $leaveBalance->leave_type_id = $this->record->leave_type_id;
-               $leaveBalance->leave_session_id = $this->record->leave_session_id;
-               $leaveBalance->used = $this->record->duration;
-               $leaveBalance->available = $number_of_allowance - $this->record->duration;
-               $leaveBalance->save();
+                //create
+                $leaveBalance = new LeaveBalance();
+                $leaveBalance->employee_id = $this->record->employee_id;
+                $leaveBalance->leave_type_id = $this->record->leave_type_id;
+                $leaveBalance->leave_session_id = $this->record->leave_session_id;
+                $leaveBalance->used = $this->record->duration;
+                $leaveBalance->available = $number_of_allowance - $this->record->duration;
+                $leaveBalance->save();
             }
         }
     }
