9e89a6847cb40c5cb8d79fc6a3ee373de37d9cdf
Devadatta Sahoo <webmaster@nettantra.com>
2023-04-03 16:53:44 +0530

    Fixing CheckinMode for non employees

--- app/Filament/Widgets/AttendanceClock.php
+++ app/Filament/Widgets/AttendanceClock.php
@@ -27,6 +27,7 @@ class AttendanceClock extends TableWidget
 
     public static function canView(): bool
     {
+        if (!auth()->user()->employee) return false;
         $check_in_mode_override = app(\App\Settings\AttendanceSettings::class)->check_in_mode_override;
         $check_in_mode = !empty($check_in_mode_override) ? $check_in_mode_override : auth()->user()->employee->check_in_mode;
         if (auth()->user()->employee && ($check_in_mode != CheckInMode::DEVICE)) {
